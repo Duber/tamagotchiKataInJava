e5ebbc2974994923857a19b4e40ad3872c4970dd
duber <miguel@miguelcruz.net>
2015-08-06 19:21:34 +0200

    DummyTest

---
+++ test/com/scbcn/TamagochiTest.java
@@ -0,0 +1,16 @@
+package com.scbcn;
+
+import junit.framework.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by duber on 06/08/15.
+ */
+public class TamagochiTest {
+    @Test
+    public void DummyTest(){
+        Assert.assertTrue(true);
+    }
+}
